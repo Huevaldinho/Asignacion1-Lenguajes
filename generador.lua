4bb0a19ab07d30a264fd6ccdc950925a699c6f09
mfss0412 <mfss0412@estudianTEC.cr>
2022-09-16 14:21:30 -0600

    yield
    Se le metio lo de la corrutina

--- generador.lua
+++ generador.lua
@@ -1,20 +1,11 @@
-/*
-Fecha Creacion: 13/09/2022
-Ultima Modificacion: 13/09/2022
-*/
+
+--Fecha Creacion: 13/09/2022
+--Ultima Modificacion: 13/09/2022
+
 
 
 function isPrime(x)
-    /*
-    Funcion:
-        Define si x es primo o no
-    Parametros:
-        int x:
-            Numero que se evaluo si es o no primo
-    Retorna:
-        boolean:
-            True si x es primo, false si no
-    */
+ 
     if x == 0 or x == 1 then --0 y 1 no son numeros primos
         return false
     elseif x == 2 then --Primer numero primo
@@ -28,31 +19,42 @@ function isPrime(x)
     return true -- No se encontraron divisores
 end
 
-function generator(num)
-    /*
-    Funcion:
-        Devuelve una lista de tamanno num con numeros primos consecutivos
-    Parametros:
-        int num:
-            Tamanno de la lista a devolver
-    Retorna:
-        lista:
-            Lista con numeros primos de tamanno num
-    */
+co = coroutine.create(function (num)
+   
     list = {}
     size = 1  
     i = 2
-    while (size <= num) do --Sale cuando list tenga tamanno num
+    while true do --Sale cuando list tenga tamanno num
         if isPrime(i) then --Si i es primo se agregaa a list
-            prime_list[size] = i --Se agrega i a list
-            size = size + 1 -- Se aumenta el contador de size de la lista
+            print(coroutine.status(co))
+            coroutine.yield(i)
+            size = size+1
         end
+        if size>num then
+            print("Sali mal lol")
+            return
+        end
+        
         i = i + 1 --Se aumenta i
     end
-    return prime_list --Retorna la lista
+    print("Sali mal lol")
+    return  --Retornamos
+end)
+
+function main(num)
+    for i=1,num do
+        -- print("Numero " , i, ": ",coroutine.resume(co, num))
+        print(coroutine.resume(co, num))
+        print(coroutine.status(co))
+    end
+    print(coroutine.status(co))
+    print("Terminamos en el main")
+    return 
 end
 
-n = 1200 --Tamanno de la lista
-lst = generator(n) --lista con primos retornada dela funcion
-print("The prime numbers in this range are: ", table.concat(lst, ","))
+main(5)
+
+-- n = 1200 --Tamanno de la lista
+-- lst = generator(n) --lista con primos retornada dela funcion
+-- print("The prime numbers in this range are: ", table.concat(lst, ","))
 
